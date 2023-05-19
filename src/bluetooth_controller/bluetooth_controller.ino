ae04b5aa29301ced8c2e0096a8b8786bb251eabc
Jason Too <jasontoo@Jasons-MacBook-Air.local>
2023-05-19 09:00:34 +0100

    Updated source code

---
+++ src/bluetooth_controller/bluetooth_controller.ino
@@ -0,0 +1,163 @@
+/*
+   -- PowerPod --
+   
+   This source code of graphical user interface 
+   has been generated automatically by RemoteXY editor.
+   To compile this code using RemoteXY library 3.1.8 or later version 
+   download by link http://remotexy.com/en/library/
+   To connect using RemoteXY mobile app by link http://remotexy.com/en/download/                   
+     - for ANDROID 4.11.1 or later version;
+     - for iOS 1.9.1 or later version;
+    
+   This source code is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.    
+*/
+#include <Wire.h>
+#include "tcpm_driver.h"
+#include "usb_pd.h"
+#include <Preferences.h>
+//////////////////////////////////////////////
+//        RemoteXY include library          //
+//////////////////////////////////////////////
+
+// RemoteXY select connection mode and include library 
+#define REMOTEXY_MODE__ESP32CORE_BLE
+#include <BLEDevice.h>
+
+#include <RemoteXY.h>
+
+// RemoteXY connection settings 
+#define REMOTEXY_BLUETOOTH_NAME "PicoTin"
+
+
+// RemoteXY configurate  
+#pragma pack(push, 1)
+uint8_t RemoteXY_CONF[] =   // 58 bytes
+  { 255,2,0,4,0,51,0,16,31,1,68,17,0,62,64,38,8,13,3,4,
+  15,16,10,35,13,26,10,48,37,25,15,15,13,26,31,79,78,0,31,79,
+  70,70,0,129,0,4,3,33,6,13,80,105,99,111,84,105,110,0 };
+  
+// this structure defines all the variables and events of your control interface 
+struct {
+
+    // input variables
+  uint8_t Voltage; // =0 if select position A, =1 if position B, =2 if position C, ... 
+  uint8_t pushSwitch_1; // =1 if state is ON, else =0 
+
+    // output variables
+  float CurrentADCVal;
+
+    // other variable
+  uint8_t connect_flag;  // =1 if wire connected, else =0 
+
+} RemoteXY;
+#pragma pack(pop);
+
+/////////////////////////////////////////////
+//           END RemoteXY include          //
+/////////////////////////////////////////////
+
+
+Preferences preferences;
+int setVoltage;
+const int usb_pd_int_pin = 10;
+const int debug_led_pin  = 3;
+const int current_pin = 2;
+
+
+// USB-C Specific - TCPM start 1
+const struct tcpc_config_t tcpc_config[CONFIG_USB_PD_PORT_COUNT] = {
+  {0, fusb302_I2C_SLAVE_ADDR, &fusb302_tcpm_drv},
+};
+void setup() 
+{
+  RemoteXY_Init (); 
+  preferences.begin("credentials", false); 
+  setVoltage = preferences.getInt("Voltage");
+  RemoteXY.Voltage = setVoltage;
+
+  Serial.begin(115200);
+  Serial.println(RemoteXY.Voltage);
+
+  pinMode(usb_pd_int_pin, INPUT);
+  pinMode(debug_led_pin, OUTPUT);
+  digitalWrite(debug_led_pin, HIGH);
+  pinMode(current_pin, INPUT);
+
+  Wire.begin(1,0);
+  Wire.setClock(400000);
+  delay(500);
+  digitalWrite(debug_led_pin, LOW);
+  tcpm_init(0);
+  delay(50);
+  pd_init(0);
+  delay(50);
+
+  if(RemoteXY.Voltage == 0)
+  {
+    pd_set_max_voltage(5000);  
+  }
+  else if(RemoteXY.Voltage == 1)
+  {
+    pd_set_max_voltage(9000);  
+  }
+  else if(RemoteXY.Voltage == 2)
+  {
+    pd_set_max_voltage(15000);  
+  }
+  else if(RemoteXY.Voltage == 3)
+  {
+    pd_set_max_voltage(20000);  
+  }
+
+  
+}
+
+void loop() 
+{ 
+  RemoteXY_Handler ();
+
+  if (RemoteXY.Voltage != setVoltage)
+  {
+    Serial.println("Voltage changed!");
+    preferences.putInt("Voltage", RemoteXY.Voltage);
+    preferences.end();
+    ESP.restart();
+  }
+  if(RemoteXY.pushSwitch_1 == 1)
+  {
+    digitalWrite(debug_led_pin, HIGH);
+  }
+  else
+  {
+    digitalWrite(debug_led_pin,LOW);
+  }
+
+  if (LOW == digitalRead(usb_pd_int_pin)) {
+      tcpc_alert(0);
+      Serial.println("PD INT PIN TRIGGRED");
+  }
+
+  pd_run_state_machine(0);
+  RemoteXY.CurrentADCVal  = analogRead(current_pin);
+
+  
+  Serial.print(pd_get_max_voltage());
+  Serial.print(", ");
+  Serial.println(RemoteXY.CurrentADCVal);
+
+  // For some reason, a delay of 4 ms seems to be best
+  // My guess is that spamming the I2C bus too fast causes problems
+  delay(4);
+
+}
+
+void pd_process_source_cap_callback(int port, int cnt, uint32_t *src_caps)
+{
+  digitalWrite(debug_led_pin, HIGH);
+
+  Serial.print("Voltage set to ");
+  Serial.println(PD_MAX_VOLTAGE_MV);
+}
--- src/usb-c-demo/usb-c-demo.ino
+++ src/usb-c-demo/usb-c-demo.ino
@@ -3,18 +3,30 @@
 #include "usb_pd.h"
 #include "OTA.h"
 #include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+#include <Preferences.h>
 
 const int usb_pd_int_pin = 10;
 const int debug_led_pin  = 3;
+const int current_pin = 2;
 WiFiManager wm;
+Preferences preferences;
 
 // USB-C Specific - TCPM start 1
 const struct tcpc_config_t tcpc_config[CONFIG_USB_PD_PORT_COUNT] = {
   {0, fusb302_I2C_SLAVE_ADDR, &fusb302_tcpm_drv},
 };
 // USB-C Specific - TCPM end 1
-
+int current = 0;
+bool streamCurrent = false;
+bool outputEnable = false;
+int voltageSet = 0;
 void setup() {
+  preferences.begin("my-app", false); 
+
+  Serial.begin(115200);
+
+
+  
     // Configure and start the WiFi station
   WiFi.mode(WIFI_STA);
 
@@ -27,28 +39,99 @@ void setup() {
   }
   setupOTAWiFiMuted("PowerPod");
   pinMode(usb_pd_int_pin, INPUT);
+  pinMode(current_pin, INPUT);
+
   pinMode(debug_led_pin, OUTPUT);
+
   digitalWrite(debug_led_pin, HIGH);
   
   Wire.begin(1,0);
   Wire.setClock(400000);
   delay(500);
   digitalWrite(debug_led_pin, LOW);
-
   tcpm_init(0);
   delay(50);
   pd_init(0);
   delay(50);
+  TelnetStream.println(preferences.getInt("Voltage"));
+  pd_set_max_voltage(preferences.getInt("Voltage"));
+
 }
 
 void loop() {  
   ArduinoOTA.handle();
+
+  current = analogRead(current_pin);
+
   if (LOW == digitalRead(usb_pd_int_pin)) {
       tcpc_alert(0);
+      // TelnetStream.println("PD INT PIN TRIGGRED");
+  }
+  switch (TelnetStream.read()) {
+    case '?':
+      debug_instructions();
+      break;
+    case 'V':
+      TelnetStream.print("Set Voltage (mV) = ");
+      TelnetStream.println(pd_get_max_voltage());
+      break;
+    case 'I':
+      TelnetStream.print("Current Draw = ");
+      TelnetStream.println(current);
+      break;
+    case 'C':
+      voltageSet += 1;
+      if(voltageSet>=4) {voltageSet = 0;};
+
+      switch(voltageSet)
+      {
+        case 0:
+          TelnetStream.println("Setting to 5V");
+          preferences.putInt("Voltage", 5000);
+          break;
+        case 1:
+          TelnetStream.println("Setting to 9V");
+          preferences.putInt("Voltage", 9000);
+          break;
+        case 2:
+          TelnetStream.println("Setting to 15V");
+          preferences.putInt("Voltage", 15000);
+          break;
+        case 3:
+          TelnetStream.println("Setting to 20V");
+          preferences.putInt("Voltage", 20000);
+          break;
+      }
+      break;
+    case 'S':
+      TelnetStream.println("Toggle Stream");
+      delay(300);
+      streamCurrent = !streamCurrent;
+      break;
+    case 'O':
+      TelnetStream.println("Toggle Output");
+      delay(300);
+      outputEnable = !outputEnable;
+      break;
+    case 'R':
+      TelnetStream.println("Restarting now");
+      ESP.restart();
   }
 
-  TelnetStream.println(pd_get_max_voltage());
-  TelnetStream.println("looping...");
+  if(streamCurrent)
+  {
+    TelnetStream.print("Current Draw = ");
+    TelnetStream.println(current);
+  }
+
+  if(outputEnable)
+  {
+    digitalWrite(debug_led_pin, HIGH);
+  }
+  else
+  {
+    digitalWrite(debug_led_pin, LOW);
+  }
   pd_run_state_machine(0);
   // For some reason, a delay of 4 ms seems to be best
   // My guess is that spamming the I2C bus too fast causes problems
@@ -57,6 +140,18 @@ void loop() {
 
 void pd_process_source_cap_callback(int port, int cnt, uint32_t *src_caps)
 {
-  digitalWrite(debug_led_pin, HIGH);
+  digitalWrite(debug_led_pin, LOW);
+
+  TelnetStream.print("Voltage set to ");
+  TelnetStream.println(PD_MAX_VOLTAGE_MV);
 }
 
+void debug_instructions()
+{
+  TelnetStream.println("V - Get UCPD voltage");
+  TelnetStream.println("I - Get current draw");
+  TelnetStream.println("C - Switch set voltage (requires restart)");
+  TelnetStream.println("S - Stream current value");
+  TelnetStream.println("O - Enable output");
+  TelnetStream.println("R - Restart");
+}
